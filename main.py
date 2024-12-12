9820a29ba13581335e757c5ae52758ee760797f5
FeliciaNemet <nemet.felicia@gmail.com>
2024-12-11 20:48:57 -0500

    Began working on the slotmachine reel algorithm

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+
+
 
 
 
